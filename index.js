e5ea9861e4010c4d3b3449338b3474a7dba1624c
Marina8520 <70947566+Marina8520@users.noreply.github.com>
2022-02-07 15:41:58 +0300

    feat: add sounds

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const logo = document.querySelector(".logo");
 const foto = document.querySelector(".main-container");
 const birds = document.querySelectorAll(".bird-name");
+const audio = document.querySelector(".audio");
+const player = document.querySelector(".play"); 
 
 let img = [
     "url(./assets/img/solovey.jpg)",
@@ -8,7 +10,17 @@ let img = [
     "url(./assets/img/zarynka.jpg)",
     "url(./assets/img/javoronok.jpg)",
     "url(./assets/img/slavka.jpg)"
-]
+];
+
+let sounds = [
+    "./assets/audio/assets_audio_solovey.mp3",
+    "./assets/audio/drozd.mp3",
+    "./assets/audio/assets_audio_zarynka.mp3",
+    "./assets/audio/javoronok.mp3",
+    "./assets/audio/slavka.mp3"
+];
+
+let pause = "url(./assets/svg/pause.svg)";
 
 logo.addEventListener("click", () => {
     birds.forEach(el => {
@@ -16,6 +28,9 @@ logo.addEventListener("click", () => {
     })
     logo.classList.add("active");  
     foto.style.backgroundImage = "url(./assets/img/forest.jpg)";
+    audio.src = "./assets/audio/forest.mp3";
+    player.classList.toggle("play-active"); 
+    playAudio();
 })
 
 console.log(birds);
@@ -29,5 +44,27 @@ for(let i = 0; i < birds.length; i++) {
         logo.classList.remove("active"); 
         birds[i].classList.add("active");  
         foto.style.backgroundImage = img[i];
+        audio.src = sounds[i];
+        player.classList.toggle("play-active"); 
+        playAudio();
 }); 
 };
+
+
+player.addEventListener("click", () => {
+    player.classList.toggle("play-active"); 
+    if(player.classList.contains("play-active") === true) {
+        playAudio(); 
+    } else {
+        pauseAudio();  
+    }
+});
+ 
+
+function playAudio() {
+    audio.currentTime = 0;
+    audio.play();
+  }
+  function pauseAudio() {
+    audio.pause();
+  };
